f402b93308381cca34d418f9c7423da73d8b044b
Coloredforest <83455113+Coloredforest@users.noreply.github.com>
2021-05-17 20:36:53 +0200

    Add files via upload

---
+++ Opdracht1/script.js
@@ -0,0 +1,11 @@
+console.log ("Hello Winc Academy")
+   
+        let numberfour = 4
+        let numberthree = 3
+
+console.log (numberfour)
+console.log (numberfour + numberfour)
+
+        let age = 32
+
+console.log (age)
