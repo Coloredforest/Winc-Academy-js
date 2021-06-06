fa6272c47e62e105fc0dc49af73a0bb1d56490fa
Coloredforest <83455113+Coloredforest@users.noreply.github.com>
2021-06-06 19:43:51 +0200

    Add files via upload

---
+++ Opdracht6/script.js
@@ -0,0 +1,35 @@
+/* lesson - making spaghetti
+
+// Writing or 'defining' a function
+
+const makeSpaghetti = function () {
+    console.log('boil water');
+    console.log('add pasta');
+    console.log('make sauce');
+    console.log('drain pasta');
+    console.log('combine pasta and sauce');
+};
+
+// Using or 'calling' a function
+
+makeSpaghetti();
+
+*/
+
+const makePizza = function () {
+    console.log('maak het deeg');
+    console.log('beleg het deeg');
+    console.log('doe de pizza in de oven');
+};
+
+const makeSushi = function () {
+    console.log('Maak rijst');
+    console.log('Doe inhoud in rijst');
+    console.log('Rol sushi');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
