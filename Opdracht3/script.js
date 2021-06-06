644c3283e2323895baa84896afab1b5967a2a53c
Coloredforest <83455113+Coloredforest@users.noreply.github.com>
2021-06-06 11:01:44 +0200

    Add files via upload

---
+++ Opdracht3/script.js
@@ -0,0 +1,33 @@
+var age = 18
+var isFemale = False
+var driverStatus = "bob"
+
+
+if (age < 18){
+
+    console.log("jij mag de kroeg niet in")
+
+} else 
+
+    console.log("je bent ouder dan 17 jaar, je mag de kroeg in als je een dame bent")
+
+if (isFemale = false){
+
+        console.log("jij mag de kroeg niet in")
+    
+} else 
+    
+        console.log("je bent een dame, als je ouder bent dan 17 jaar mag je de kroeg in")
+
+
+if (driverStatus = "bob"){
+
+    console.log("Jij mag rijden")
+
+} else {
+
+    console.log("Jij mag niet rijden")
+
+}
+
+// node script.js
