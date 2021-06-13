3f09d4e5842f12477966f9a354f25478c70ca859
Coloredforest <83455113+Coloredforest@users.noreply.github.com>
2021-06-13 10:40:24 +0200

    Add files via upload

---
+++ Opdracht7/script.js
@@ -0,0 +1,12 @@
+const verven = function (wall, color) {
+    console.log('The ' + wall + ' wall has been painted ' + color);
+};
+
+const color = ('green');
+const wall = ('north');
+
+
+verven (wall, color);
+
+
+
