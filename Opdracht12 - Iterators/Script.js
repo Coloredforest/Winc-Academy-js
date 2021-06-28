6766737ba244ebc37efa443c27631c9bac652dfa
Coloredforest <83455113+Coloredforest@users.noreply.github.com>
2021-06-28 20:16:54 +0200

    Add files via upload

---
+++ Opdracht12 - Iterators/Script.js
@@ -0,0 +1,36 @@
+const color = ['yellow', 'blue', 'red', 'orange'];
+
+/*
+while (i = 0; i <=3; i++) {
+    console.log(color[i]);
+}
+
+
+for (i = 0; i <=3; i++) {
+    console.log(color[i]);
+}
+
+*/
+
+color.forEach(function test (item,index) {
+    console.log()
+}
+]
+
+
+/*
+
+    1. Hoeveel regels neemt mijn for loop en mijn while loop in beslag?
+    ->1
+
+    2. Hoeveel regels neemt mijn forEach method in beslag?
+    -1
+    
+    3. Welke voordelen heeft het gebruiken van een array method nog meer t.o.v. een for of een while loop?
+    Minder foutgevoelig 
+    Welke methode vind je bijvoorbeeld makkelijker leesbaar? Waarom?
+    Loop is uitgebreider geschreven, geeft dus meer informatie. foreach is compacter / netter in totaal codering.
+   
+    4. Kun je een array method gebruiken op een object? Het antwoord is nee.
+
+    */
